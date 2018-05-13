e57c7d384784a5ff81f235f8f00b93944677de45
Takuya_AKIYAMA <Takuya_AKIYAMA@LAPTOP-D232M19L>
2018-05-13 19:47:53 +0900

    2nd commit test

--- test.py
+++ test.py
@@ -9,8 +9,8 @@ import numpy as np
 import matplotlib.pyplot as plt
  
 # 乱数を生成
-x = np.random.rand(100)
-y = np.random.rand(100)
+x = np.random.rand(200)
+y = np.random.rand(200)
  
 # 散布図を描画
 # plt.scatter(x, y)
