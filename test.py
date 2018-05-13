6ee9d80c18fd98278d7d529200dae8c34fa673c6
Takuya_AKIYAMA <Takuya_AKIYAMA@LAPTOP-D232M19L>
2018-05-13 19:43:06 +0900

    test

---
+++ test.py
@@ -0,0 +1,23 @@
+'''
+Created on 2018/02/17
+
+@author: Takuya_AKIYAMA
+'''
+print("Hello Python!")
+
+import numpy as np
+import matplotlib.pyplot as plt
+ 
+# 乱数を生成
+x = np.random.rand(100)
+y = np.random.rand(100)
+ 
+# 散布図を描画
+# plt.scatter(x, y)
+# plt.show()
+
+# plt.scatter(x, y, s=600, c="pink", alpha=0.5, linewidths="2",edgecolors="red")
+# plt.show()
+
+plt.scatter(x, y, s=600, c="yellow", marker="*", alpha=0.5, linewidths="2", edgecolors="orange")
+plt.show()
